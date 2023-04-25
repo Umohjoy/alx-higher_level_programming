e08129a39ce1adaf4d5a8b8de192f2bf6771f4d7
Umohjoy <umohjoy27@gmail.com>
2023-04-25 20:59:00 +0000

    updtes

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,6 @@
+t fs = require('fs');
+const args = process.argv;
+
+fs.writeFile(args[2], args[3], 'utf-8', (err, data) => {
+  if (err) { console.log(err); }
+});
---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,7 @@
+t request = require('request');
+const args = process.argv;
+
+request(args[2], function (error, response, body) {
+  if (error) { console.log(error); }
+  console.log('code:', response && response.statusCode);
+});
